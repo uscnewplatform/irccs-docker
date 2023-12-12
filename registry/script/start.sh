5b34abc9aba7f853be7a5275fa84905423748a39
Turboli Salvatore <salvatore.turboli@infocube.it>
2023-12-12 17:12:37 +0100

    fix start.sh and added elastisearch

--- registry/script/start.sh
+++ registry/script/start.sh
@@ -3,7 +3,7 @@
 COMPONENTPATH=/app/quarkus-run.jar
 
 until [ \
-  "$(curl -s -w '%{http_code}' -o /dev/null "http://fhir-server:8080/fhir/metadata")" \
+  "$(curl -s -w '%{http_code}' -o /dev/null "http://fhir:8080/fhir/metadata")" \
   -eq 200 ]
 do
   echo "I'm waiting for the FHIR Server is up...";
